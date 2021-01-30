9f4e2afe4c79f5c35eab0322b7d7d3abc420f062
Pasan Madusanka <pasan@seneru.com>
2021-01-30 09:51:34 +0530

    add employee id

--- dao/src/main/java/com/createvision/wijaya_sports/valuesObject/EmployeeVO.java
+++ dao/src/main/java/com/createvision/wijaya_sports/valuesObject/EmployeeVO.java
@@ -1,6 +1,7 @@
 package com.createvision.wijaya_sports.valuesObject;
 
 public class EmployeeVO {
+    private Long id;
     private String firstName;
     private  String lastName;
     private  String address;
@@ -12,6 +13,9 @@ public class EmployeeVO {
     private String designation;
     private String mobileNumber;
     private  String landPhoneNumber;
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
 
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
