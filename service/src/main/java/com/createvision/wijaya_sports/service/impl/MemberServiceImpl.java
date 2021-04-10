d9b03288574402a08ab69c08b09f10ab88f3b031
Pasan Madusanka <pasan@seneru.com>
2021-04-10 19:30:12 +0530

    validate null values

--- service/src/main/java/com/createvision/wijaya_sports/service/impl/MemberServiceImpl.java
+++ service/src/main/java/com/createvision/wijaya_sports/service/impl/MemberServiceImpl.java
@@ -40,7 +40,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public MemberVO createNewMember(MemberVO memberVO) throws Exception {
         try {
-            Member member=new Member();
+            Member member = new Member();
             Gender gender = genderDao.get(memberVO.getGenderId());
             member.setFirstName(memberVO.getFirstName());
             member.setLastName(memberVO.getLastName());
@@ -50,27 +50,27 @@ public class MemberServiceImpl implements MemberService {
             member.setGender(gender);
             member.setJoinDate(commonFunction.getDateTimeByDateString(memberVO.getJoinedDate()));
             member.setSports(Sports.GYM);
-            Measurement saveMeasurement =new Measurement();
-            for (MeasurementVO measurementVO:memberVO.getMeasurement()) {
-                Measurement measurement =new Measurement();
-                measurement.setAbdomen(Double.toString(measurementVO.getAbdomen())!=null?Double.toString(measurementVO.getAbdomen()):"");
-                measurement.setChest(Double.toString(measurementVO.getChest())!=null?Double.toString(measurementVO.getChest()):"");
-                measurement.setHips(Double.toString(measurementVO.getHips())!=null?Double.toString(measurementVO.getHips()):"");
-                measurement.setLeftCalf(Double.toString(measurementVO.getLeft_calf())!=null?Double.toString(measurementVO.getLeft_calf()):"");
-                measurement.setRightCalf(Double.toString(measurementVO.getRight_calf())!=null?Double.toString(measurementVO.getRight_calf()):"");
-                measurement.setLeftThigh(Double.toString(measurementVO.getLeft_thigh())!=null?Double.toString(measurementVO.getLeft_thigh()):"");
-                measurement.setWrist(Double.toString(measurementVO.getWrist())!=null?Double.toString(measurementVO.getWrist()):"");
+            Measurement saveMeasurement = new Measurement();
+            for (MeasurementVO measurementVO : memberVO.getMeasurement()) {
+                Measurement measurement = new Measurement();
+                measurement.setAbdomen(Double.toString(measurementVO.getAbdomen()) != null ? Double.toString(measurementVO.getAbdomen()) : "");
+                measurement.setChest(Double.toString(measurementVO.getChest()) != null ? Double.toString(measurementVO.getChest()) : "");
+                measurement.setHips(Double.toString(measurementVO.getHips()) != null ? Double.toString(measurementVO.getHips()) : "");
+                measurement.setLeftCalf(Double.toString(measurementVO.getLeft_calf()) != null ? Double.toString(measurementVO.getLeft_calf()) : "");
+                measurement.setRightCalf(Double.toString(measurementVO.getRight_calf()) != null ? Double.toString(measurementVO.getRight_calf()) : "");
+                measurement.setLeftThigh(Double.toString(measurementVO.getLeft_thigh()) != null ? Double.toString(measurementVO.getLeft_thigh()) : "");
+                measurement.setWrist(Double.toString(measurementVO.getWrist()) != null ? Double.toString(measurementVO.getWrist()) : "");
                 Long saveId = measurementDao.save(measurement);
-                saveMeasurement =measurementDao.get(saveId);
+                saveMeasurement = measurementDao.get(saveId);
 
             }
             member.setMeasurement(saveMeasurement);
-            RegistrationFee saveRegistrationFee =new RegistrationFee();
-            for (RegistrationFeeVO rgis :memberVO.getRegistrationFee()) {
-                RegistrationFee registrationFee =new RegistrationFee();
+            RegistrationFee saveRegistrationFee = new RegistrationFee();
+            for (RegistrationFeeVO rgis : memberVO.getRegistrationFee()) {
+                RegistrationFee registrationFee = new RegistrationFee();
                 registrationFee.setAmount(rgis.getAmount());
                 registrationFee.setDescription(rgis.getDescription());
-              //  registrationFee.setDate(commonFunction.getDateTimeByDateString(rgis.getDate()));
+                //  registrationFee.setDate(commonFunction.getDateTimeByDateString(rgis.getDate()));
                 Long saveId = registrationFreeDao.save(registrationFee);
                 saveRegistrationFee = registrationFreeDao.get(saveId);
             }
@@ -78,20 +78,17 @@ public class MemberServiceImpl implements MemberService {
             member.setRegistrationFee(saveRegistrationFee);
 
 
-
             if (memberVO.getSportId() == 1) {
                 member.setSports(Sports.BADMINTON);
-            }
-            else if (memberVO.getSportId() == 2){
+            } else if (memberVO.getSportId() == 2) {
                 member.setSports(Sports.SWIMMING);
-            }
-            else {member.setSports(Sports.GYM);
+            } else {
+                member.setSports(Sports.GYM);
             }
             System.out.println("Come to this");
             memberDao.save(member);
 
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             throw e;
         }
         return memberVO;
@@ -157,7 +154,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public List<MemberVO> getAllMemberDetails() throws Exception {
-        List<MemberVO>memberVOList =new ArrayList<>();
+        List<MemberVO> memberVOList = new ArrayList<>();
 
         try {
             List<Member> memberList = memberDao.getAll();
@@ -175,27 +172,30 @@ public class MemberServiceImpl implements MemberService {
                 Gender gender = genderDao.get(member.getGender().getId());
 
 
-
-                List<MeasurementVO> measurementVOList =new ArrayList<>();
                 //   measurementVO.setRight_arm(Double.parseDouble(member.getMeasurement().getRightArm()));
-                MeasurementVO measurementVO =new MeasurementVO();
-                measurementVO.setAbdomen(Double.parseDouble(member.getMeasurement().getAbdomen()));
-                measurementVO.setChest(Double.parseDouble(member.getMeasurement().getChest()));
-                measurementVO.setHips(Double.parseDouble(member.getMeasurement().getHips()));
-                measurementVO.setLeft_calf(Double.parseDouble(member.getMeasurement().getLeftCalf()));
-                measurementVO.setLeft_thigh(Double.parseDouble(member.getMeasurement().getLeftThigh()));
-                measurementVO.setRight_calf(Double.parseDouble(member.getMeasurement().getRightCalf()));
-                measurementVO.setWrist(Double.parseDouble(member.getMeasurement().getWrist()));
-                measurementVOList.add(measurementVO);
-                memberVO.setMeasurement(measurementVOList);
 
-                List<RegistrationFeeVO> registrationFeeVOList =new ArrayList<>();
-                RegistrationFeeVO registrationFeeVO =new RegistrationFeeVO();
-                registrationFeeVO.setAmount(member.getRegistrationFee().getAmount());
-                registrationFeeVO.setDescription(member.getRegistrationFee().getDescription());
-                registrationFeeVOList.add(registrationFeeVO);
-                memberVO.setRegistrationFee(registrationFeeVOList);
+                if (member.getMeasurement() != null) {
+                    List<MeasurementVO> measurementVOList = new ArrayList<>();
+                    MeasurementVO measurementVO = new MeasurementVO();
+                    measurementVO.setAbdomen(Double.parseDouble(member.getMeasurement().getAbdomen()!=null?member.getMeasurement().getAbdomen():"0.00"));
+                    measurementVO.setChest(Double.parseDouble(member.getMeasurement().getChest()!=null?member.getMeasurement().getChest():"0.00"));
+                    measurementVO.setHips(Double.parseDouble(member.getMeasurement().getHips()!=null?member.getMeasurement().getHips():"0.00"));
+                    measurementVO.setLeft_calf(Double.parseDouble(member.getMeasurement().getLeftCalf()!=null?member.getMeasurement().getLeftCalf():"0.00"));
+                    measurementVO.setLeft_thigh(Double.parseDouble(member.getMeasurement().getLeftThigh()!=null?member.getMeasurement().getLeftThigh():"0.00"));
+                    measurementVO.setRight_calf(Double.parseDouble(member.getMeasurement().getRightCalf()!=null?member.getMeasurement().getRightCalf():"0.00"));
+                    measurementVO.setWrist(Double.parseDouble(member.getMeasurement().getWrist()!=null?member.getMeasurement().getWrist():"0.00"));
+                    measurementVOList.add(measurementVO);
+                    memberVO.setMeasurement(measurementVOList);
+                }
 
+                if (member.getRegistrationFee() != null) {
+                    List<RegistrationFeeVO> registrationFeeVOList = new ArrayList<>();
+                    RegistrationFeeVO registrationFeeVO = new RegistrationFeeVO();
+                    registrationFeeVO.setAmount(Double.toString(member.getRegistrationFee().getAmount())!=null?member.getRegistrationFee().getAmount():0.00);
+                    registrationFeeVO.setDescription(member.getRegistrationFee().getDescription()!=null?member.getRegistrationFee().getDescription():"");
+                    registrationFeeVOList.add(registrationFeeVO);
+                    memberVO.setRegistrationFee(registrationFeeVOList);
+                }
 
 
                 memberVO.setGenderId(gender.getId());
@@ -204,7 +204,7 @@ public class MemberServiceImpl implements MemberService {
             }
 
             return memberVOList;
-        }catch (Exception e){
+        } catch (Exception e) {
 
             throw e;
         }
